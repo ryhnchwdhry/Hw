34a5ddda08a4c0beb11e0eb1ffda330e017eb1dc
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-06-02 16:16:48 -0400

    Abstract Class

---
+++ NursingSchool.java
@@ -0,0 +1,9 @@
+package hw8Abstraction;
+
+public abstract class NursingSchool {
+	public abstract void hygiene();
+
+	public void caring() {
+		System.out.println("This is non abstract method");
+	}
+}
