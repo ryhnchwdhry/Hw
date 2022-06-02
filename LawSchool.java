e0915ef1c183afd2442f00771da5f480c85f7ec0
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-06-02 16:13:23 -0400

    interface

---
+++ LawSchool.java
@@ -0,0 +1,5 @@
+package hw8Abstraction;
+
+public interface LawSchool {
+	public void lawInfo();
+}
---
+++ University.java
@@ -0,0 +1,11 @@
+package hw8Abstraction;
+
+public interface University {
+	public void classSize();
+
+	public void playGround();
+
+	public void teacher();
+
+//	public University() {} cannot have a constructor within an interface in Java
+}
