a91581c5bfb2ba47df7a12cb6ec4b77b208e6469
ryhnchwdhry <104733459+ryhnchwdhry@users.noreply.github.com>
2022-06-02 16:18:46 -0400

    regular Class

---
+++ ColumbiaUniversity.java
@@ -0,0 +1,11 @@
+package hw8Abstraction;
+
+public class ColumbiaUniversity {
+	public void chemistry() {
+		System.out.println("This is non abstract class");
+	}
+
+//	public abstract void biology (); A class that is declared using “abstract” keyword is known as abstract class
+	public ColumbiaUniversity() {
+	}
+}
---
+++ NYUniversity.java
@@ -0,0 +1,12 @@
+package hw8Abstraction;
+
+public class NYUniversity {
+	public void anthropology() {
+		System.out.println("This is non abstract method");
+
+	}
+	public static void main(String[] args) {
+		NYUniversity nYUniversity = new NYUniversity();
+		nYUniversity.anthropology();
+	}
+}
